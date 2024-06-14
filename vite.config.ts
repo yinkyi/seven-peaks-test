6144b36cbc61878ddf39ce19c6162d955f7a96f6
Yin <yinyin.k@rznet.com>
2024-06-14 21:21:37 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from '@svgr/rollup';
+
+/** https://vitejs.dev/config */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    mode: mode,
+    plugins: [react(), svgr()],
+    server: {
+      port: parseInt(env.VITE_APP_PORT),
+      host: true,
+    },
+    preview: {
+      port: parseInt(env.VITE_APP_PORT),
+    },
+    css: {
+      preprocessorOptions: {
+        // scss: {
+        //   additionalData: `
+        //                   @import  "./src/_variables.scss";
+        //                 `,
+        // },
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 1250,
+      rollupOptions: {
+        output: [
+          {
+            manualChunks: {
+              antd: ['antd'],
+              antd_icon: ['@ant-design/icons'],
+              axios: ['axios'],
+              redux: ['@reduxjs/toolkit'],
+            },
+          },
+        ],
+      },
+    },
+  };
+});
