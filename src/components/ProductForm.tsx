71f083653825438af69807bbefaed5f61ce4f1c8
Yin <yinyin.k@rznet.com>
2024-06-16 14:04:11 +0700

    list css

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { productAction } from '../store/product';
 import { IProduct, IinitialState } from '../interface';
@@ -6,6 +6,7 @@ const ProductForm: React.FC = () => {
   const products = useSelector((state: IinitialState) => state.item.products);
   const dispatch = useDispatch();
   const [productName, setProductName] = useState<string>('');
+  const productRef = useRef<HTMLInputElement>(null);
 
   const addProductFunction = (event: React.FormEvent) => {
     event.preventDefault();
@@ -15,12 +16,15 @@ const ProductForm: React.FC = () => {
         fav: true,
       }),
     );
+    setProductName('');
+    productRef.current?.focus();
   };
 
   return (
     <div>
       <form onSubmit={addProductFunction}>
         <input
+          ref={productRef}
           name='product_name'
           value={productName}
           placeholder='input product name'
@@ -30,10 +34,21 @@ const ProductForm: React.FC = () => {
           Submit
         </button>
       </form>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'block' }}>
         {products &&
           products.map((product: IProduct) => {
-            return <h1>{product.name}</h1>;
+            return (
+              <div
+                style={{
+                  margin: '2px',
+                  border: 'solid  1px',
+                  padding: '10px',
+                  borderRadius: '12px',
+                }}
+              >
+                {product.name}
+              </div>
+            );
           })}
       </div>
     </div>
